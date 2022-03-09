bdb1a310c8d329c5d8a698fcf6b71ee36e6d73ac
dev-shovon13 <ahmedshovoncse13@gmail.com>
2022-03-09 19:00:32 +0600

    dynamic sub array average with k=3

--- educative/2_pattern-sliding-window/introduction/brute-force.js
+++ educative/2_pattern-sliding-window/introduction/brute-force.js
@@ -1,8 +1,15 @@
 let output = [];
-// const array =[1, 3, 2, 6, -1, 4, 1, 8, 2]
-const array = [1, 3, 2, 1];
-// const k=5
-for (let i = 0; i < array.length - 1; i++) {
-  const avg = (array[i] + array[i + 1]) / 2;
-  console.log(avg);
+const array = [1, 3, 2, 6, -1, 4, 1, 8, 2];
+// const array = [1, 3, 2, 1, 6];
+let sum = 0;
+let avg = 0;
+const k = 3;
+for (let i = 0; i < array.length - (k - 1); i++) {
+  for (let j = 0; j < k; j++) {
+    sum += array[i + j];
+  }
+  avg = sum / k;
+  output.push(avg);
+  sum = 0;
 }
+console.log(output);
